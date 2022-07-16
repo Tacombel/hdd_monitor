b6b0fa1dd7c2bd4ecdc28bea023ec170e79fb7aa
Daniel <tacombel@gmail.com>
2022-07-16 22:27:06 +0200

    Added config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+class Config(object):
+    token = `Your long life token given by HA`
+    server = 'Cerebro'
+    disks = ['HDD1', 'HDD2']
+    paths = ['/dev/sdb', '/dev/sdc']
+    HA_server= 'IP'
