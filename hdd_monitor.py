ced08eb92e3e116b8fa28aa6152c6b94132b1a61
Daniel <tacombel@gmail.com>
2022-07-16 22:25:52 +0200

    First functional version

--- hdd_monitor.py
+++ hdd_monitor.py
@@ -1,9 +1,34 @@
-import os
+# python 3.8.10
 
-hddtemp = os.popen('hddtemp /dev/sdb /dev/sdc').readlines()
+from config import Config
+import os
+import json
+from requests import post
+
+
+cmd = "hddtemp"
+for e in Config.paths:
+    cmd = cmd + " " + e
+hddtemp = os.popen(cmd).readlines()
 print(hddtemp)
+disks = Config.disks
+
+n = 0
 for e in hddtemp:
     temp = e.split()[2]
     size = len(temp)
-    temp = temp[:size-2]
-    print(temp)
+    temp_data = {"attributes": {"unit_of_measurement": "°C"}}
+    temp_data["state"] = temp[:size-2]
+    url = "http://" + Config.HA_server + ":8123/api/states/sensor." + Config.server + "_temp_" + disks[n]
+    print(url)
+    headers = {
+        "Content-Type": "application/json",
+    }
+    headers["Authorization"] = "Bearer " + Config.token
+    data = json.dumps(temp_data)
+    response = post(url, headers=headers, data=data)
+
+    print(response.text)
+    print(response)
+    n +=1
+    
